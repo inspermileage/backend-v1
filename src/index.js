8725498044f9985cfc0aa5557e4a000ef056fd09
Martim <martimfj@al.insper.edu.br>
2019-07-17 03:12:46 -0300

    Vuln fix

--- src/index.js
+++ src/index.js
@@ -11,19 +11,19 @@ const app = express()
 const server = require("http").Server(app)
 const io = require("socket.io")(server)
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
 })
+
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
   req.io = io
   next()
 })
-
-app.use(cors())
 
 app.use("/files", express.static(path.resolve(__dirname, "..", "datasets")))
 
